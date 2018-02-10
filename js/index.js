5ec1e25e633096c63417da4114efc49cebd536ac
skuralov10 <skuralov@mail.ru>
2018-02-10 18:41:29 +0100

    редакция 2е тз

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
-var name = 'Stas';
-
-if (name == 'Stas') {
-    console.log('да');
+function div (a,b) {
+    return a/b;
 }
+var result = div (10,2);
+console.log (result);
+
+function plus (a,b) {
+    return a+b;
+}
+var result = plus (10,5);
+console.log (result);
