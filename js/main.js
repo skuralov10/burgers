7e8b00a5fbeb633506557048b9eab6eb88922a9e
skuralov10 <skuralov@mail.ru>
2018-02-14 16:46:11 +0100

    one page scroll

--- js/main.js
+++ js/main.js
@@ -26,10 +26,13 @@ $(document).ready(function(){
     });
 
     $('.team-accordeon__trigger').on('click', function(e){
-        // e.preventDefault();
         $('.team-accordeon__trigger').css({
-            'color' : "yellow"
+            'color' : "#f9b43b"
         });
+
+        // $('.team-accordeon__trigger').css({
+        //     'transform' : 'rotate(180deg)'        
+        // });
     });
 
 
@@ -49,4 +52,156 @@ $(document).ready(function(){
         $('.menu-accordeon__item_dieta').toggleClass('menu-accordeon__item_active');
     });
 
+
+    //maps 
+    ymaps.ready(init);
+    var myMap;
+
+    function init(){     
+        myMap = new ymaps.Map("maps", {
+            center: [46.48, 30.73],
+            zoom: 14,
+            controls: []
+        });
+
+        myMap.behaviors.disable('scrollZoom');
+
+
+        var coords = [
+            [46.48, 30.73], [46.48, 30.75], [46.47, 30.72]
+        ],
+            myCollection = new ymaps.GeoObjectCollection({}, {
+            draggable: false,
+            iconLayout: 'default#image',
+            iconImageHref: '../img/icons/map-marker.png',
+            iconImageSize: [46, 57],
+            iconImageOffset: [-26, -52]
+            });
+        
+        for (var i = 0; i < coords.length; i++) {
+            myCollection.add(new ymaps.Placemark(coords[i]));
+        }
+        
+        myMap.geoObjects.add(myCollection);
+    }
 });
+
+
+// $('.menu-accordeon__item').on('click', function(e){
+//     e.preventDefault();
+//     $('.menu-accordeon__item').toggleClass('active')
+//         .siblings().removeClass('active');
+// });
+
+
+// $(document).ready(function() {
+// 	$('.main-content').fullpage();
+// });
+
+
+
+//one page scroll
+
+const sections = $('.section');
+const dislpay = $('.main-content');
+let isScroll = false;
+
+const mobileDetect = new MobileDetect(window.navigator.userAgent);
+const isMobile = mobileDetect.mobile();
+
+const setActiveMenuItem = itemEq => {
+    $('.nav-bar__item').eq(itemEq).addClass('active')
+    .siblings().removeClass('active');
+}
+
+const performTransition = sectionEq => {
+    const position = `${sectionEq * -100}%`;
+
+    if (isScroll) return
+
+    isScroll = true;
+
+    sections.eq(sectionEq).addClass('active')
+    .siblings().removeClass('active');
+    
+    dislpay.css({
+        'transform' : `translate(0, ${position})`,
+        "-webkit-transform" : `translate(0, ${position})`
+    })
+
+    setTimeout(() => {
+        isScroll = false;
+
+        setActiveMenuItem(sectionEq);
+
+    }, 1300); //время анимации  
+}
+
+const scrollToSection = direction => {
+    const activeSection = sections.filter('.active');
+    const nextSection = activeSection.next();
+    const prevSection = activeSection.prev();
+
+    if (direction === 'up' && prevSection.length) {
+        performTransition(prevSection.index())
+    }
+
+    if (direction === 'down' && nextSection.length) {
+        performTransition(nextSection.index())
+    }
+}
+
+
+$(document).on({
+    wheel: e => {
+        const deltaY = e.originalEvent.deltaY;
+        const direction = deltaY > 0 ? "down" : "up";
+
+        scrollToSection(direction);
+    },
+
+    // //листаем вниз
+    // if (deltaY > 0) {
+    //     scrollToSection('down');
+    //     // performTransition(3);
+    // }
+
+    // //листаем вверх
+    // if (deltaY < 0) {
+    //     scrollToSection('up');
+    // }
+
+    keydown: e => {
+        switch (e.keyCode){
+            case 40:
+                scrollToSection('down');
+                break;
+            case 38:
+                scrollToSection('up');
+                break;
+        }
+    },
+
+    touchmove: e => e.preventDefault()
+});
+
+//nav
+$('[data-scrool-to]').on('click', e =>{
+    e.preventDefault();
+
+    const target = parseInt($(e.currentTarget).attr('data-scrool-to'));
+
+    performTransition(target);
+})
+
+
+//touch swipe
+if (isMobile) {
+    $(document).swipe( {
+        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+            //плагин возвращает фактическое значение, а нам нужно на оборот
+            const scrollDirection = direction === 'down' ? 'up' : 'down';
+            scrollToSection(scrollDirection);
+        }
+    });
+}
